c6e7de6d6993ba6728abccca764b073421f08d08
Decimo-10 <117395262+Decimo-10@users.noreply.github.com>
2023-01-13 11:45:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const markRead = document.querySelector(".header__mark-read");
-const unreadMes = document.querySelectorAll(".notif--unread");
-const notifPoints = document.querySelectorAll(".notif__point");
-const notifNum = document.querySelector(".header__notif-number");
+const markRead = document.querySelector(".header__mark-read");/* Store reference to "Mark all read" */
+const unreadMes = document.querySelectorAll(".notif--unread");/* Unread messages */
+const notifPoints = document.querySelectorAll(".notif__point");/* Small red dots in the unread messages */
+const notifNum = document.querySelector(".header__notif-number");/* Notification counter at the top*/
 console.log(unreadMes);
 markRead.addEventListener("click", () =>{
     for(const mes of unreadMes){
